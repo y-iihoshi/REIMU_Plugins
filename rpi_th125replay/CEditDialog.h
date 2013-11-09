d354e6ff9910cb5abb78ed5e318e213c8b02cc5f
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2013-11-10 01:32:38 +0900

    Add rpi_th125replay project

---
+++ rpi_th125replay/CEditDialog.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Windows.h>
+#include "..\rpi_threplay\CDialogBase.h"
+
+INT_PTR OpenEditDialog(HINSTANCE hInstance, HWND hWndParent, LPCTSTR lpszFilename);
+
+class CEditDialog : public CDialogBase
+{
+private:
+	typedef CDialogBase base;
+
+	LPCTSTR lpszFilename;
+	const int nMaxTextSize;
+
+public:
+	CEditDialog(LPCTSTR lpszFilename, int nMaxTextSize = 30000);
+	~CEditDialog();
+
+	INT_PTR Show(HINSTANCE hInst, HWND hWndOwner);
+	INT_PTR DialogProc(UINT uMsg, WPARAM wParam, LPARAM lParam);
+	BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam);
+	void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+	void OnClose(HWND hwnd);
+};

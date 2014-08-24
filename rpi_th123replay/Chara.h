140ed2aa2f0f4902f3c8fe7df2ffc0628431c0de
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2014-08-24 12:39:25 +0900

    Add rpi_th123replay project

---
+++ rpi_th123replay/Chara.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Th123
+{
+	enum class Chara : std::uint8_t
+	{
+		Reimu,
+		Marisa,
+		Sakuya,
+		Alice,
+		Patchouli,
+		Youmu,
+		Remilia,
+		Yuyuko,
+		Yukari,
+		Suika,
+		Reisen,
+		Aya,
+		Komachi,
+		Iku,
+		Tenshi,
+		Sanae,
+		Cirno,
+		Meiling,
+		Utsuho,
+		Suwako
+	};
+}
---
+++ rpi_th123replay/StringMaps.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <sstream>
+#include <cstdint>
+#include "Mode.h"
+#include "Chara.h"
+
+namespace Th123
+{
+	template<typename TKey>
+	static std::string GetString(const std::map<TKey, std::string> &stringMap, const TKey &key)
+	{
+		try
+		{
+			return stringMap.at(key);
+		}
+		catch (const std::out_of_range &)
+		{
+			std::ostringstream stream;
+			stream << "unknown (" << static_cast<int>(key) << ")";
+			return stream.str();
+		}
+	}
+
+	extern const std::map<std::uint16_t, std::string> Versions;
+	extern const std::map<Th123::Mode, std::string> Modes;
+	extern const std::map<Th123::Chara, std::string> Characters;
+	extern const std::map<std::uint16_t, std::string> SystemCards;
+	extern const std::map<Th123::Chara, std::map<std::uint16_t, std::string>> Cards;
+	extern const std::map<std::uint8_t, std::string> Stages;
+	extern const std::map<std::uint8_t, std::string> Bgms;
+}
